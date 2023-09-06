2cffef2173c02b31a87659945c374cfff81d75ba
{chiidera} <{cassandraebulue@gmail.com}>
2023-09-06 20:42:55 +0100

    debug

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings' #added for azure
+# settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings' #added for azure
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')  #original settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module) #added for azure
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')  #original settings
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module) #added for azure
 
 application = get_wsgi_application()
